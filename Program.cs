53aec3b662cb8f70b8e4255af5d123752b844ce6
BFam <longlivelado@gmail.com>
2025-08-16 12:54:39 -0500

    conflict resolved

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("input your full name");
 Console.WriteLine("input your address");
-<<<<<<< HEAD
 Console.WriteLine("What your is the of your school?");
 Console.WriteLine("Enter the name of your pet");
-=======
 Console.WriteLine("What your is the name of your school?");
->>>>>>> 89cb2921694bd3ca9cdb994fbb5e8f73908a561f
+
