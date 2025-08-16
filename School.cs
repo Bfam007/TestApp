c55ad1506914d592a8c5f465ba8db355e2127374
BFam <longlivelado@gmail.com>
2025-08-16 12:27:37 -0500

    school class added

--- School.cs
+++ School.cs
@@ -2,4 +2,9 @@ public class School
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Subject { get; set; }
+    public int Age { get; set; }
+    public string HomeAddress { get; set; }
+    
+
 }
