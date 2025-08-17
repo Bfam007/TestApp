f025a08fc449f5d01ca6b78ca1590972454c0830
BFam <longlivelado@gmail.com>
2025-08-17 13:30:52 -0500

    names of family members

--- Program.cs
+++ Program.cs
@@ -5,4 +5,7 @@ Console.WriteLine("input your address");
 Console.WriteLine("What your is the of your school?");
 Console.WriteLine("Enter the name of your pet");
 Console.WriteLine("What your is the name of your school?");
+Console.WriteLine("Enter the names of all you imidiate family members");
+Console.WriteLine("Enter the name of your bestfreiend");
+Console.WriteLine("What is your American dream like");
 
--- obj/Debug/net9.0/TestApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55ad1506914d592a8c5f465ba8db355e2127374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53aec3b662cb8f70b8e4255af5d123752b844ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
